57f7ba293735318d0e51f5ddfa45d95dc53cf1a7
CarlosT4 <ctc3486>
2025-03-07 12:49:25 -0500

    comentarios esquema de vacunacion

--- src/utils/vaccineUtils.js
+++ src/utils/vaccineUtils.js
@@ -12,23 +12,93 @@ AMA:
 VARICELA 
 HEPATITIS 
 
-1. Menos de un anio
-[0 - 1] meses -> BCG (tuberculosis), HvB (hepatitis b)
-[2 - 3] meses -> Rotavirus, Antineumocócica, Penta, IPV 
-[4 - 5] meses -> Rotavirus, Antineumococica, Penta 2º (Pentavalente), IPV  
-[6 - 6] meses -> Penta 3º, IPV, Influenza Ped. 
-[7 - 11] meses -> Influenza Ped.  
+----Menos de un anio
+[0 - 1] meses -> DOSIS UNICA BCG (tuberculosis), DOSIS UNICA HvB (hepatitis b)
+[2 - 3] meses -> 1RA DOSIS Penta, 1RA DOSIS IPV, 1RA DOSIS Rotavirus, 1RA DOSIS Antineumocócica 
+[4 - 5] meses -> 2DA DOSIS Penta, 2DA DOSIS IPV, 2DA DOSIS Rotavirus, 2DA DOSIS Antineumococica 
+[6 - 6] meses -> 3RA DOSIS Penta, 3RA DOSIS IPV, 1RA DOSIS Influenza Pediatrica 
+[7 - 11] meses -> 2DA DOSIS Influenza Pediatrica
 
-2. 1anio
-[12 - 14] meses -> Neumococo, Influenza Ped. ,SPR 1º dosis, Varicela 
-[15 - 17] meses ->  AMA (fiebre amarilla), Hepatitis "A" 
-[18 - 23] meses -> IPV(2024) (poliomielitits), SPR 2º dosis (sarampion), DPT 1º ref. 
+----1anio
+[12 - 14] meses -> 1RA DOSIS SPR, 3RA DOSIS Antineumococica , DOSIS UNICA Varicela, DOSIS ANUAL Influenza Ped.  
+[15 - 17] meses -> DOSIS UNICA AMA (fiebre amarilla), DOSIS UNICA  Hepatitis A 
+[18 - 23] meses -> 1RA DOSIS DE REFUERZO IPV, 1RA DOSIS DE REFUERZO DPT, 2DA DOSIS SRP
 
-3. 2anios en adelante
-[2 - 3] anios -> IPV(2024), ., SPR 2º dosis, DPT 1º ref. 
-[4 - 8] anios -> Influenza Adul., Apo 2º ref poliomielitits. (2023), DPT 2° ref. (dipteria - tos convulsiva) 
-[9 - 13] anios -> VPH
-60 - mas anios -> Influenza, Neumococo
+---2anios en adelante
+[2 - 3] anios -> 1RA DOSIS DE REFUERZO IPV, 1RA DOSIS DE REFUERZO DPT, 2DA DOSIS SRP, DOSIS ANUAL Influenza Ped. 
+[4 - 8] anios -> DOSIS ANUAL Influenza Ped., 2DA DOSIS DE REFUERZO DPT (dipteria - tos convulsiva), 2DA DOSIS DE REFUERZO APO (poliomielitits), 
+[9 - 13] anios -> VPH UNA DOSIS 
+[14 - 18] anios -> VPH UNA DOSIS SI NO SE APLICO ANTES
+[19 - 59] anios -> Vacuna antiamarílica, Hepatitis B, Influenza adulto
+60 - mas anios -> DOSIS ANUAL Influenza, DOSIS UNICA Antineumococica
+
+- Recién nacidos (OK)
+Dosis única de la vacuna contra el bacilo de Calmette-Guérin (BCG): contra las formas graves de tuberculosis (TB).
+Dosis única de la vacuna contra el virus de Hepatitis B (HVB): contra la hepatitis B.
+
+- A los 2 meses 
+Primera dosis de la vacuna pentavalente (DPT - HvB - Hib): vacuna combinada de 5 componentes que previene la difteria, tétanos, tos ferina (DPT), neumonía, meningitis por haemophilus tipo b y hepatitis B.
+Primera dosis de la vacuna antipolio inactivada inyectable (IPV): vacuna contra la parálisis flácida aguda.
+Primera dosis de la vacuna contra el rotavirus: vacuna que previene las diarreas por rotavirus.
+Primera dosis de la vacuna antineumocócica: vacuna que protege contra la neumonía, meningitis y otitis media.
+
+- A los 4 meses
+Segunda dosis de la vacuna pentavalente (DPT - HvB - Hib): vacuna combinada de 5 componentes que previene la difteria, tétanos, tos ferina (DPT), neumonía, meningitis por haemophilus tipo b y hepatitis B.
+Segunda dosis de la vacuna antipolio inactivada inyectable (IPV): vacuna contra la parálisis flácida aguda.
+Segunda dosis de la vacuna contra el rotavirus: vacuna que previene las diarreas por rotavirus.
+Segunda dosis de la vacuna antineumocócica: vacuna que protege contra la neumonía, meningitis y otitis media.
+
+- A los 6 meses 
+Tercera dosis de la vacuna pentavalente (DPT - HvB - Hib): vacuna combinada de 5 componentes que previene la difteria, tétanos, tos ferina (DPT), neumonía, meningitis por haemophilus tipo b y hepatitis B.
+Tercera dosis de la vacuna antipolio oral (IPV): protección contra la parálisis flácida aguda.
+Primera dosis de la vacuna contra la influenza pediátrica: vacuna contra la gripe y bronquitis.
+
+- A los 7 meses
+Segunda dosis de la vacuna contra la influenza pediátrica: vacuna contra la gripe y bronquitis.
+
+- A los 12 meses
+Primera dosis de la vacuna contra el sarampión, paperas y rubéola (SPR).
+Tercera dosis de la vacuna antineumocócica: protección contra la neumonía, meningitis y otitis media.
+Dosis única de la vacuna contra la varicela.
+Dosis anual de la vacuna contra la influenza pediátrica: vacuna contra la gripe y bronquitis.
+
+- A los 15 meses
+Dosis única de la vacuna antiamarílica (AMA): contra la fiebre amarilla.
+Dosis única de la vacuna contra el virus de la hepatitis A.
+
+- A los 18 meses
+Primera dosis de refuerzo de la vacuna de la difteria, pertussis y tétanos (DPT): vacuna que previene la difteria, tétanos y tos ferina.
+Primera dosis de refuerzo de la vacuna antipolio oral (IPV): protección contra la parálisis flácida aguda.
+Segunda dosis de la vacuna contra el sarampión, paperas y rubéola (SPR).
+
+- A los 2, 3 y 4 años
+2 años: Dosis anual de la vacuna contra la influenza pediátrica.
+3 años: Dosis anual de la vacuna contra la influenza pediátrica.
+4 años:
+Dosis anual de la vacuna contra la influenza pediátrica.
+Segunda dosis de refuerzo de la vacuna de la difteria, pertussis y tétanos (DPT): vacuna que previene la difteria, tétanos y tos ferina.
+Segunda dosis de refuerzo de la vacuna antipolio oral (APO): vacuna contra la parálisis flácida aguda.
+
+- De 9 a 13 años
+Vacuna contra el virus del papiloma humano (VPH): Prevención del cáncer de cuello uterino y verrugas genitales.
+Niñas: una dosis.
+Niños: una dosis.
+
+- De 14 a 18 años
+Vacuna contra el virus del papiloma humano (VPH): Prevención del cáncer de cuello uterino y verrugas genitales.
+Mujeres: una dosis, siempre y cuando no se haya aplicado antes.
+Hombres: una dosis, siempre y cuando no se haya aplicado antes.
+
+- De 19 a 59 anios
+Vacuna antiamarílica: Contra la fiebre amarilla (para personas de 15 meses a 59 años que viajen a áreas de riesgo por turismo o trabajo, con un mínimo de 10 días previos al viaje, como para residentes en zonas endémicas).
+Vacuna contra la hepatitis B: Para personas con condiciones de comorbilidad.
+Vacuna contra la influenza: Para personas con condiciones de comorbilidad.
+
+
+- De 60 anios a mas
+Dosis anual de la vacuna contra la influenza.
+Dosis única de la vacuna antineumocócica: protección contra la neumonía, meningitis y otitis media.
+
 */
 export const esquemaVacunacion = [
   { maxAnios: 0, minMeses: 0, maxMeses: 1, vacunas: ["BCG (Tuberculosis)", "HvB (Hepatitis b)"] },
